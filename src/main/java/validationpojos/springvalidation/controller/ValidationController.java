bc03ca83023769d9a0a2d13299b3abdcfd2a8efd
Pedro Henrique <pedroheenri_q@hotmail.com>
2021-12-02 20:39:35 -0300

    acrescenta tratamento de execao

--- src/main/java/validationpojos/springvalidation/controller/ValidationController.java
+++ src/main/java/validationpojos/springvalidation/controller/ValidationController.java
@@ -1,11 +1,16 @@
 package validationpojos.springvalidation.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import validationpojos.springvalidation.model.ClienteModel;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/cliente")
@@ -14,6 +19,20 @@ public class ValidationController {
     @PostMapping("/salvar")
     private ResponseEntity<String> salvarCliente(@RequestBody @Valid ClienteModel clienteModel){
         return ResponseEntity.ok("");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handlerValidationException(MethodArgumentNotValidException exception){
+        Map<String,String> errors = new HashMap<>();
+        exception.getBindingResult()
+                .getAllErrors()
+                .forEach(erro -> {
+                    String fieldName =((FieldError)erro).getField();
+                    String errorMessage= erro.getDefaultMessage();
+                    errors.put(fieldName,errorMessage);
+                });
+        return  errors;
     }
 
 }
