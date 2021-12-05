6e9b2a0f365a7628abfdb6d9748b606fec2bb07b
Pedro Henrique <pedroheenri_q@hotmail.com>
2021-12-05 17:32:00 -0300

    adiciona end point de consulta e salvamento de usuario

--- src/main/java/validationpojos/springvalidation/controller/ValidationController.java
+++ src/main/java/validationpojos/springvalidation/controller/ValidationController.java
@@ -1,24 +1,46 @@
 package validationpojos.springvalidation.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import validationpojos.springvalidation.model.ClienteModel;
+import validationpojos.springvalidation.model.UserModel;
+import validationpojos.springvalidation.repository.UserRepository;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("api/user")
 public class ValidationController {
 
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public ValidationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @PostMapping("/salvar")
     private ResponseEntity<ClienteModel> salvarCliente(@RequestBody @Valid ClienteModel clienteModel){
         return ResponseEntity.ok(clienteModel);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<UserModel>> listTodos(){
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<UserModel>salvar(@RequestBody UserModel user){
+        return ResponseEntity.ok(userRepository.save(user));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
