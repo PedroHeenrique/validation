ff8313c4e9cf07ca5854539526f3722e1c9f358e
Pedro Henrique <pedroheenri_q@hotmail.com>
2021-12-05 17:59:07 -0300

    criptografa senha antes de ser salva no banco, adiciona end point para validacao de senha.

--- src/main/java/validationpojos/springvalidation/controller/ValidationController.java
+++ src/main/java/validationpojos/springvalidation/controller/ValidationController.java
@@ -4,6 +4,7 @@ package validationpojos.springvalidation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/user")
@@ -22,10 +24,12 @@ public class ValidationController {
 
 
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     @Autowired
-    public ValidationController(UserRepository userRepository) {
+    public ValidationController(UserRepository userRepository, PasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     @PostMapping("/salvar")
@@ -40,7 +44,25 @@ public class ValidationController {
 
     @PostMapping("/save")
     public ResponseEntity<UserModel>salvar(@RequestBody UserModel user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+                                                @RequestParam String password){
+
+        Optional<UserModel> optUser = userRepository.findByLogin(login);
+        if(optUser.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+
+       boolean valid = encoder.matches(password, optUser.get().getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(status).body(valid);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
